3e97dafd0a16e1941ee75ba7bc4c6f487bb82845
arrrrto <artotopp@gmail.com>
2015-08-31 16:15:08 +0800

    Add IsAnagram function

--- snippets/snippets/Program.cs
+++ snippets/snippets/Program.cs
@@ -16,6 +16,10 @@ namespace snippets
 
             solution.IsPalindrome("rat", "car");
             solution.IsPalindrome("anagram", "margana");
+
+            solution.IsAnagram("anagram", "nagaram");
+            solution.IsAnagram("rat", "car");
+            solution.IsAnagram("arto toppi", "port patio");
             Console.ReadLine();
         }
     }
@@ -51,6 +55,42 @@ namespace snippets
                 return true;
             else
                 return false;
+        }
+
+        public bool IsAnagram(string s, string t)
+        {
+            
+            s = s.Replace(" ", "");
+            t = t.Replace(" ", "");
+
+            char[] array1 = s.ToCharArray();
+            char[] array2 = t.ToCharArray();
+
+            if (array1.Length != array2.Length)
+                return false;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        array2 = array2.Where((source, index) => index != j).ToArray();
+                        continue;
+                    }
+                }
+            }
+
+            if (array2.Length != 0)
+            {
+                Console.WriteLine("Not anagram");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Is anagram");
+                return true;
+            }
         }
     }
 }
